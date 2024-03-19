8cb903024ec12818ae4926e0c0936062ebc4129c
Tyler Rockwood <rockwood@redpanda.com>
2024-03-19 13:54:42 +0000

    Add clang wrappers
    Signed-off-by: Tyler Rockwood <rockwood@redpanda.com>

--- dot_zshrc.d/01_base.sh
+++
@@ -1,8 +0,0 @@
-alias ls="eza"
-alias vi="nvim"
-alias vim="nvim"
-
-
-autoload edit-command-line; zle -N edit-command-line
-# Press escape+v to edit the current zsh command in vim ♥︎
-bindkey -M vicmd v edit-command-line
