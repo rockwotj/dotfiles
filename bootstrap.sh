64146f4476447bbf922891ba992f60d0425a2eea
Tyler Rockwood <rockwotj@gmail.com>
2026-04-18 13:05:37 +0000

    Add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install git build-essential
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" < /dev/null
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+BINDIR=$HOME/.local/bin/ sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply rockwotj
+echo '/home/linuxbrew/.linuxbrew/bin/zsh' | sudo tee /etc/shells
+(cd ~/.local && python3 -m venv venv && ./venv/bin/pip install pynvim)
+npm install -g neovim basedpyright
+
